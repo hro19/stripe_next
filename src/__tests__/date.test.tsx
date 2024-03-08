4bcc6981b068760357bf24cd5d5016a9b0a99ef6
hro19 <fourbasic19@gmail.com>
2024-03-08 16:19:04 +0900

    営業時間内外のテストを追加

---
+++ src/__tests__/date.test.tsx
@@ -0,0 +1,36 @@
+const sut = {
+    purchase: () => {
+        const businessHours = [9, 17]
+        const currentHour = new Date().getHours()
+        const [open, close] = businessHours
+
+        if (currentHour > open && currentHour < close)
+            return { message: 'Success' }
+
+        return { message: 'Error' }
+    }
+}
+
+describe(`購入フローのテスト`, () => {
+    beforeEach(() => {
+        // Mockを使う
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        // Mockではなくリアルタイムを使う
+        vi.useRealTimers()
+    })
+
+    it(`営業時間内の場合、purchase()はサクセスを返す`, () => {
+        const date = new Date(2000, 1, 1, 13)
+        vi.setSystemTime(date)
+        expect(sut.purchase()).toEqual({ message: 'Success' })
+    })
+
+    it(`営業時間外の場合、purchase()はエラーを返す`, () => {
+        const date = new Date(2000, 1, 1, 19)
+        vi.setSystemTime(date)
+        expect(sut.purchase()).toEqual({ message: 'Error' })
+    })
+})
